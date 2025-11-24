4e4b5bea208d368f6258bf2ca57022d70a034305
Dario Casertano <dario.casertano@gmail.com>
2025-11-24 15:59:20 +0100

    ✨ Aggiungi comando per invio email di prova
    - **Creazione** della classe `Sendmail` nel namespace `App\Console\Commands` per implementare un comando Artisan (`app:sendmail`) dedicato all'invio di email di prova.
    - **Configurazione** della descrizione e della firma del comando per una documentazione chiara.
    - **Definizione** dello scheletro del metodo `handle` per raccogliere input, validare i dati, e inviare l'email.
    
    Nessuna modifica alle configurazioni esistenti o dipendenze, nessun impatto sulla compatibilità.

---
+++ app/Console/Commands/Sendmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Sendmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sendmail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Invio e-mail di prova';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // La funzioen $this->ask() permette di chiedere all'utente di inserire un valore
+        // $var = $this->ask()
+
+        // Validare tutti i campi
+
+        // Invia e-mail
+
+        return Command::SUCCESS;
+    }
+}
